caa64e477c97237074acf2f88e4d2da7ab21a928
Lynda Santiago <you@example.com>
2020-09-01 18:51:11 -0400

    created two gets, post and delete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const server = express();
+server.use(express.json());
 const port = 8000;
 let users = [
   { id: 1, name: "Lynda", bio: "I like fried chicken" },
@@ -11,11 +12,23 @@ server.get("/api/users", (req, res) => {
     .status(500)
     .json({ errorMessage: "The users information could not be retrieved." });
 });
+server.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  getUser = users.find((user) => user.id === id);
+  res.status(200).json({ data: getUser });
+});
 server.post("/api/users", (req, res) => {
   const body = req.body;
+  console.log(req.body);
   users.push(body);
   res.status(201).json({ data: users });
 });
+server.delete('api/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const deleteUser = users.filter(user => user.id !== id)
+    res.status(204).end();
+    res.status(200).json(deleteUser)
+})
 server.listen(port, () => {
   console.log("server up");
 });
